1605109ae54653046752088e77af437cad441330
sumonDev9 <sumonmit678@gmail.com>
2025-03-10 21:19:09 +0530

    Add Socket.IO setup to Express server for real-time communication

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 const http = require('http');
 const expressServer = http.createServer(app);
 
-
+const {Server} = require('socket.io');
+const io = new Server(expressServer);
 
 
 app.get('/', (req, res) => {
