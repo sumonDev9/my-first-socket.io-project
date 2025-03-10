7c3f43058b6f9bb1075488c08b920bb3d3f4dbb0
sumonDev9 <sumonmit678@gmail.com>
2025-03-10 21:11:34 +0530

    Add basic Express server setup with route serving index.html

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const expressServer = http.createServer(app);
+
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+"/index.html")
+})
+
+
+expressServer.listen(3000, () => {
+    console.log('Server runing')
+})
